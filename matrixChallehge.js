57218a75c85594214ea0e0ecc09516b60c57b494
Ruben Villalvazo Manríquez <vim.ruben@hotmail.com>
2022-07-23 15:54:55 -0300

    Add a problem description

--- matrixChallehge.js
+++ matrixChallehge.js
@@ -1,3 +1,18 @@
+/*
+***************************************************************************************************************************************************
+Matrix Challenge Have the function MatrixChallenge (strArr) 
+read strArr which will be an array consisting of the locations of a Queen and King on a standard 8x8 chess board with no other pieces on the board. 
+The structure of strArr will be the following: ["(x1,71)"," (x2,y2)"] 
+with (x1,y1) representing the position of the queen and (x2,y2) representing the location of the king with x and y ranging from 1 to 8. 
+Your program should determine if the king is in check based on the current positions of the pieces, and if so, 
+return the number of spaces it can move to in order to get out of check. 
+If the king is not in check, return -1. For example: if strArr is ["(4,4)"."(6,6)") then your program should output 6. 
+Remember, because only the queen and king are on the board, if the queen is checking the king by being directly adjacent to it, 
+technically the king can capture the queen. Examples Input: {"(1,1)", "(1,4)"} Output: 3 Input: {"(3,1)","(4,4)") Output: -1
+***************************************************************************************************************************************************
+*/
+
+
 const MatrixChallenge = (strArr) => {
     /* 
     **********************
